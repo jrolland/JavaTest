f214067edefe8e733b811d2d77df42e41b097ef0
julien <julien@cocoricodes.com>
2022-12-21 15:27:16 +0100

    premier jet

--- main.java
+++ main.java
@@ -1,7 +1,23 @@
 public class main {
+    private static int num=25;
+
+    static void maMeth()
+    {
+        System.out.println("Toto !!!");
+    }
+
     public static void main(String[] args){
         int data=10;
+        long monLong=1278;
+        float monFloat=123.56f;
+        double monDouble=122343444;
         System.out.println(data);
         System.out.println("Yo man !");
+        System.out.println(num);
+        maMeth();
+        System.out.println(monLong);
+        System.out.println(monFloat);
+        System.out.println(monDouble);
     }
 }
+
